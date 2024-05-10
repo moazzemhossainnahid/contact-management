a8becba1d8879933d90363719ed3da50db7d1568
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2024-05-10 12:50:35 +0600

    initiate redux

---
+++ contact-management-client/src/redux/api/apiSlice.ts
@@ -0,0 +1,69 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+
+// Create base query for the original base URL
+const baseQuery = fetchBaseQuery({
+    baseUrl: `http://localhost:5000/api/v1/`,
+});
+
+export const api = createApi({
+    reducerPath: "api",
+    baseQuery,
+    tagTypes: [
+        "addContact",
+        "updateContact",
+        "deleteContact",
+        "getAllContacts",
+        "getContact"
+    ],
+
+    endpoints: (builder) => ({
+        // Contacts
+
+        getAllContacts: builder.query({
+            query: ({
+                page,
+                limit,
+            }) =>
+                `/contacts?page=${page}&limit=${limit}`,
+            providesTags: ["getAllContacts"],
+        }),
+        getSingleContact: builder.query({
+            query: (id) => `/contacts/${id}`,
+            providesTags: ["getContact"],
+        }),
+
+        addContact: builder.mutation({
+            query: ({ data }) => ({
+                url: "/contacts/create-contact",
+                method: "POST",
+                body: data,
+            }),
+            invalidatesTags: ["addContact"],
+        }),
+        updateContact: builder.mutation({
+            query: ({ id, data }) => ({
+                url: `/contacts/${id}`,
+                method: "PATCH",
+                body: data,
+            }),
+            invalidatesTags: ["updateContact"],
+        }),
+        deleteContact: builder.mutation({
+            query: ({ _id }) => ({
+                url: `/contacts/${_id}`,
+                method: "DELETE",
+            }),
+            invalidatesTags: ["deleteContact"],
+        }),
+
+    }),
+});
+
+export const {
+    useGetAllContactsQuery,
+    useGetSingleContactQuery,
+    useAddContactMutation,
+    useUpdateContactMutation,
+    useDeleteContactMutation,
+} = api;
